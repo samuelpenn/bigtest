3440b7f0e0a66937564a431dea61480248375bad
Samuel Penn <sam@glendale.org.uk>
2020-04-01 14:47:34 +0100

    Added 100K files

---
+++ src/100k/mkfiles.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+base=$1
+num=$2
+
+i=0
+while [ $i -lt $num ]
+do
+	echo $(( $num - $i)) 
+	a=$(( $i % 10))
+	b=$(( $i % 100))
+	c=$(( $i % 1000))
+	d=$(( $i / 10000))
+
+	mkdir -p $base/$d/$c/$b/$a
+	lorem -w 2 | sed 's/ //g'  > $base/$d/$c/$b/$a/$i.txt
+
+	i=$(( $i + 1 ))
+
+done
+
